16a8b3c43a0a4c26125a8554bf2e1789da170747
peterr <coderiot@zoho.com>
2014-07-18 14:52:24 +0200

    parsing .csv or .tsv files with informations about new mappings articles

---
+++ src/main/java/org/dbpedia/mappings/missingbot/util/ParseCSV.java
@@ -0,0 +1,36 @@
+package org.dbpedia.mappings.missingbot.util;
+
+import au.com.bytecode.opencsv.CSVReader;
+import au.com.bytecode.opencsv.bean.ColumnPositionMappingStrategy;
+import au.com.bytecode.opencsv.bean.CsvToBean;
+import org.dbpedia.mappings.missingbot.create.Record;
+
+import java.io.*;
+import java.util.List;
+
+/**
+ * Created by peterr on 16.07.14.
+ */
+public class ParseCSV {
+
+    public static List<Record> parseCreationFile(String path, String delimiter) throws IOException {
+        ColumnPositionMappingStrategy<Record> strategy = new ColumnPositionMappingStrategy<Record>();
+        strategy.setType(Record.class);
+
+        String[] columns = new String[] {"category", "name", "template", "url"};
+        strategy.setColumnMapping(columns);
+
+        CSVReader reader = new CSVReader(new FileReader(path), ',', '\'', 1);
+        CsvToBean<Record> csv = new CsvToBean<Record>();
+        return csv.parse(strategy, reader);
+    }
+
+    public static List<Record> parseCreationCSV(String path) throws IOException {
+       return parseCreationFile(path, ",");
+    }
+
+    public static List<Record> parseCreationTSV(String path) throws IOException {
+        return parseCreationFile(path, "\t");
+    }
+
+}
