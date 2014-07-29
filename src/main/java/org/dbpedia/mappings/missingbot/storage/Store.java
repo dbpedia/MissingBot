225c0ad15433c975c165f8f35b51440e5f5e5165
peterr <coderiot@zoho.com>
2014-07-29 17:56:06 +0200

    Add new storage functions for rest service
     * remove entries from database
     * list all entries by language
     * make initStore static for passing jdbc url

--- src/main/java/org/dbpedia/mappings/missingbot/storage/Store.java
+++ src/main/java/org/dbpedia/mappings/missingbot/storage/Store.java
@@ -1,6 +1,11 @@
-package org.dbpedia.mappings.missingbot.db;
+package org.dbpedia.mappings.missingbot.storage;
 
+import org.dbpedia.mappings.missingbot.rest.bean.Missing;
+
+import java.io.IOException;
 import java.sql.*;
+import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * Created by peterr on 04.06.14.
@@ -8,21 +13,13 @@ import java.sql.*;
 public class Store {
 
     private Connection conn = null;
-    private String jdbc_url;
+    private static String JDBC_URL;
 
-    public Store(String jdbc_url) throws SQLException {
-        // TODO: jedesmal eine neue Connection ist wohl sicherer, wenn mehrere Clients anfragen
-//        this.conn = DriverManager.getConnection("jdbc:h2:~/db/missing_translation");
-        this.jdbc_url = jdbc_url;
-        initStore();
-    }
+    public static void initStore(String jdbc_url) {
+        JDBC_URL = jdbc_url;
 
-    public void initStore(){
-        // TODO: Missing Labels abgleichen
         try {
-            Connection conn = DriverManager.getConnection(this.jdbc_url);
-//            conn_pool = JdbcConnectionPool.create(JDBC_URL, "", "");
-//            Connection conn = conn_pool.getConnection();
+            Connection conn = DriverManager.getConnection(JDBC_URL);
             Statement stat = conn.createStatement();
 //            stat.execute("DROP TABLE missing IF EXISTS");
 
@@ -42,8 +39,7 @@ public class Store {
 
     public void put(String title, String label, String translation, String language) {
         try {
-            Connection conn = DriverManager.getConnection(this.jdbc_url);
-//            Connection conn = conn_pool.getConnection();
+            Connection conn = DriverManager.getConnection(JDBC_URL);
             PreparedStatement stat = conn.prepareStatement("MERGE INTO missing VALUES(?, ?, ?, ?)");
 
             stat.setString(1, title);
@@ -59,4 +55,48 @@ public class Store {
         }
     }
 
+    public void remove(String title, String language) {
+        try {
+            Connection conn = DriverManager.getConnection(JDBC_URL);
+            PreparedStatement stat = conn.prepareStatement("DELETE FROM missing WHERE title=? and language=?");
+            stat.setString(1, title);
+            stat.setString(2, language);
+
+            stat.execute();
+
+            stat.close();
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public Map<String, Missing> getAllByLang(String language) throws IOException {
+        Map<String, Missing> store = new TreeMap<>();
+
+        try {
+            Connection conn = DriverManager.getConnection(JDBC_URL);
+            PreparedStatement stat = conn.prepareStatement("SELECT * FROM missing WHERE language=?");
+            stat.setString(1, language);
+            ResultSet rs = stat.executeQuery();
+
+            while(rs.next()) {
+                store.put(rs.getString("title"), new Missing(rs.getString("title"),
+                        rs.getString("label"),
+                        rs.getString("translation"),
+                        language));
+            }
+
+            rs.close();
+            stat.close();
+            conn.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return store;
+
+    }
 }
